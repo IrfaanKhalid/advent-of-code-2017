420c014c3cbc9640382eb81ff31896117e3b7eaa
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2017-12-05 00:22:03 -0600

    Added day03Test.java for when I get around to it

---
+++ src/test/day03Test.java
@@ -0,0 +1,15 @@
+/*
+ * Irfaan Khalid
+ * 12/5/2017
+ *
+ * Description: JUnit tests for Day 03's challenge in Advent of Code 2017.
+ *              http://adventofcode.com/2017/day/3
+ */
+
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class day03Test {
+
+}
